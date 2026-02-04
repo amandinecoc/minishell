440a69be2121e612ff3279cf8ea36a32a54a18f3
AmandineCoc <amandine@DESKTOP-EQ9C6UK.localdomain>
2026-02-04 19:23:09 +0100

    idée de forme 1.0.3

--- readline.c
+++ readline.c
@@ -6,7 +6,7 @@
 /*   By: amandine <amandine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 17:52:25 by amandine          #+#    #+#             */
-/*   Updated: 2026/02/04 19:14:23 by amandine         ###   ########.fr       */
+/*   Updated: 2026/02/04 19:22:38 by amandine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ int main()
 {
     char *line;
     
-    line = readline("minishell> ");
     while (1)
     {
+        line = readline("minishell>");  //gestion line
         if (line != NULL)
             add_history(line);
-        if (ft_strncmp(line, "clear", 6) == 0)
+        if (ft_strncmp(line, "clear", 6) == 0)  //parsing
         {
             rl_clear_history();
             printf("Historique effacé.\n");
         }
-        printf("Vous avez tapé: %s\n", line);
+        printf("Vous avez tapé: %s\n", line);  //executable
         free(line);
         line = readline("minishell> ");
     }
-    printf("Fin\n");
+    printf("Fin\n");  //clear et free tout en cas de ctrl d
     return 0;
 }
