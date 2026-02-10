3accad5e000e3a374b5a54300db5aa16594f2ed1
AmandineCoc <amandine@DESKTOP-EQ9C6UK.localdomain>
2026-02-10 12:04:20 +0100

    while(1) 1.1.0

--- readline.c
+++ readline.c
@@ -6,7 +6,7 @@
 /*   By: amandine <amandine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 17:52:25 by amandine          #+#    #+#             */
-/*   Updated: 2026/02/04 19:22:38 by amandine         ###   ########.fr       */
+/*   Updated: 2026/02/10 12:03:52 by amandine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,17 @@ int main()
     
     while (1)
     {
-        line = readline("minishell>");  //gestion line
+        line = readline("minishell>");
         if (line != NULL)
             add_history(line);
-        if (ft_strncmp(line, "clear", 6) == 0)  //parsing
+        if (ft_strncmp(line, "clear", 6) == 0)
         {
             rl_clear_history();
             printf("Historique effacé.\n");
         }
-        printf("Vous avez tapé: %s\n", line);  //executable
+        printf("Vous avez tapé: %s\n", line);
         free(line);
-        line = readline("minishell> ");
     }
-    printf("Fin\n");  //clear et free tout en cas de ctrl d
+    printf("Fin\n");
     return 0;
 }
